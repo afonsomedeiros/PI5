0c6ed6ffb6633cd78b4f5c7ded20ecf101fb9a41
afonsomedeiros <afonso.b.medeiros@gmail.com>
2023-11-16 21:46:46 -0300

    fix port

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
 
     bottle.TEMPLATE_PATH.insert(0, f"{ROOT_PATH}/app/views/")
     
-    core.run(debug=True, reloader=True)
+    core.run(debug=True, reloader=True, port=10000)
